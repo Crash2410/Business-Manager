4bea6fe554e957bf9da9a299e63f7af1086f5be1
Artem <artem.xolod.90@gmail.com>
2020-09-02 20:23:36 +0300

    Адаптация блоков

--- src/js/script.js
+++ src/js/script.js
@@ -10,5 +10,32 @@ $(document).ready(function () {
       '<button type="button" class="slick-prev" aria-label="Previous"><img src="./icons/arrow_white_right.png" alt=""></button>',
     nextArrow:
       '<button type="button" class="slick-next" aria-label="Next"><img src="./icons/arrow_white_left.png" alt=""></button>',
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          arrows: false,
+          draggable: false,
+        },
+      },
+    ],
+  });
+});
+
+window.addEventListener("DOMContentLoaded", () => {
+  const menu = document.querySelector(".menu"),
+    menuItem = document.querySelectorAll(".menu_item"),
+    hamburger = document.querySelector(".hamburger");
+
+  hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("hamburger_active");
+    menu.classList.toggle("menu_active");
+  });
+
+  menuItem.forEach(item => {
+    item.addEventListener("click", () => {
+      hamburger.classList.toggle("hamburger_active");
+      menu.classList.toggle("menu_active");
+    });
   });
 });
