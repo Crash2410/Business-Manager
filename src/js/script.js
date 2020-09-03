06d74bf6ff3b059f0a1d304bf83426760c45db28
Artem <artem.xolod.90@gmail.com>
2020-09-03 19:06:09 +0300

    Верстка 4, 5,6,7 и 8 блока

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+  //slider header
+
   $(".slider__inner").slick({
     infinite: true,
     dots: true,
@@ -10,6 +12,47 @@ $(document).ready(function () {
       '<button type="button" class="slick-prev" aria-label="Previous"><img src="./icons/arrow_white_right.png" alt=""></button>',
     nextArrow:
       '<button type="button" class="slick-next" aria-label="Next"><img src="./icons/arrow_white_left.png" alt=""></button>',
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          arrows: false,
+          draggable: false,
+        },
+      },
+    ],
+  });
+
+  // Tabs
+
+  $("ul.solutions__tabs").on(
+    "click",
+    "li:not(.solutions__tab_active)",
+    function () {
+      $(this)
+        .addClass("solutions__tab_active")
+        .siblings()
+        .removeClass("solutions__tab_active")
+        .closest("div.container")
+        .find("div.solutions__content")
+        .removeClass("solutions__content_active")
+        .eq($(this).index())
+        .addClass("solutions__content_active");
+    }
+  );
+
+  // Slider in Tabs
+
+  $(".slider__solutions").slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    draggable: false,
+    speed: 500,
+    prevArrow:
+      '<button type="button" class="slick-prev" aria-label="Previous"><img src="./icons/arrow_right.png" alt=""></button>',
+    nextArrow:
+      '<button type="button" class="slick-next" aria-label="Next"><img src="./icons/arrow_left.png" alt=""></button>',
     responsive: [
       {
         breakpoint: 1200,
